1701e13eda1aafec33f328b84c17d40a7334bd40
SumitMakwana <sumit.mak674@gmail.com>
2022-06-29 18:56:14 +0530

    initial commit

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
 from flask import Flask, render_template, request, url_for
+import mysql.connector
+
+conn = mysql.connector.connect(host='localhost', user='root', password='',database='app')
+cursor = conn.cursor()
+
+
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -11,9 +18,14 @@ def signin():
 
 @app.route('/login_validation', methods=['POST'])
 def login_validation():
-    username = request.form.get('username')
-    password = request.form.get('password')
-    return "The email is {} and the password is {}".format(username, password)
+    user = request.form.get('username')
+    pass1 = request.form.get('password')
+
+    cursor.execute("""SELECT * FROM register WHERE 'username' LIKE 'user' AND 'password' LIKE 'pass1'""")
+    app = cursor.fetchall()
+    print(app)
+    return 'hello'
+
 
 if __name__ == '__main__':
     app.run(debug = True)
