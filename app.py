4c6dea979bf412fafb8167fc2cb7faaf71087068
SumitMakwana <sumit.mak674@gmail.com>
2022-06-30 20:02:04 +0530

    initial commit

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
+from colorama import Cursor
 from flask import Flask, render_template, request, url_for
-import mysql.connector
-
-conn = mysql.connector.connect(host='localhost', user='root', password='',database='app')
-cursor = conn.cursor()
-
-
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+
+app.config['MYSQL_HOST']= 'localhost'
+app.config['MYSQL_USER']= 'root'
+app.config['MYSQL_PASSWORD']= ''
+app.config['MYSQL_DB']= 'app'
+mysql = MySQL(app)
 
 @app.route('/')
 def login():
@@ -18,14 +20,20 @@ def signin():
 
 @app.route('/login_validation', methods=['POST'])
 def login_validation():
-    user = request.form.get('username')
-    pass1 = request.form.get('password')
-
-    cursor.execute("""SELECT * FROM register WHERE 'username' LIKE 'user' AND 'password' LIKE 'pass1'""")
-    app = cursor.fetchall()
-    print(app)
+    user1 = request.form.get('username1')
+    pass1 = request.form.get('password1')
     return 'hello'
 
+@app.route('/signup_form', methods=['POST'])
+def signup_form():
+    user2 = request.form.get('username2')
+    pass2 = request.form.get('password2')
+
+    cur = mysql.connection.cursor()
+    cur.execute("INSERT INTO register (username, password) VALUES (%s, %s)", (user2, pass2))
+    mysql.connection.commit()
+    cur.close() 
+    return ("username is {} and password is {}".format(user2, pass2))
 
 if __name__ == '__main__':
     app.run(debug = True)
